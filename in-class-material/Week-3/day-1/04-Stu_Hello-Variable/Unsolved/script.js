7b63bcb865b04ebf909ceaace8eb1a5d04131c21
khinoj <kristopher.hinojosa@gmail.com>
2022-10-19 19:32:31 -0400

    class week 3 day 1

---
+++ in-class-material/Week-3/day-1/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,17 @@
+// Write Your JavaScript Code Here
+var personName= "Jack";
+var pets = 2;
+var funFact = "My cat is not fat"   
+
+
+console.log("My name is " + personName);
+console.log("I have " + pets + " pets" );
+console.log("fun fact: " + funFact);
+
+personName = "Reese";
+pets = 0;
+funFact= "I am a TA"
+
+console.log("My name is " + personName);
+console.log("I have " + pets + " pets" );
+console.log("fun fact: " + funFact)
