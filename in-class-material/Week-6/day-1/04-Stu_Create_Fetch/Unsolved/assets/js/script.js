a3f5957822d78160a5640e55ceb07d255542931c
khinoj <kristopher.hinojosa@gmail.com>
2022-11-20 22:45:08 -0500

    update fetch

--- in-class-material/Week-6/day-1/04-Stu_Create_Fetch/Unsolved/assets/js/script.js
+++ in-class-material/Week-6/day-1/04-Stu_Create_Fetch/Unsolved/assets/js/script.js
@@ -9,16 +9,20 @@ function getApi() {
 
   fetch(requestUrl)
     .then(function (response) {
+      console.log(response);
       return response.json(); //.json turns url into javascript behind the scenes.
     })
     .then(function (data) { //data is a made up var for json data
       //looping over the fetch response and inserting the URL of your repos into a list
       for (var i = 0; i < data.length; i++) {
+        console.log(data);
         //Create a list element
         var listItem = document.createElement('li');
 
         //Set the text of the list element to the JSON response's .html_url property
-        listItem.textContent = data[i].html_url;
+        listItem.textContent = data[i].html_url; //html inside data 
+        console.log(listItem);
+
 
         //Append the li element to the id associated with the ul element.
         repoList.appendChild(listItem);
